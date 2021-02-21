d0621a5c12c54e63db2f849159af239633bc3177
Marcelo Benites <marcelo.benites@hey.com>
2021-02-21 18:40:13 +0000

    Replace rule by scenario in tests

--- conference-android/app/src/androidTest/java/com/droidcon/conference/ConferenceEndToEndTest.kt
+++ conference-android/app/src/androidTest/java/com/droidcon/conference/ConferenceEndToEndTest.kt
@@ -1,29 +1,23 @@
 package com.droidcon.conference
 
-import android.content.Intent
+import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.rule.ActivityTestRule
 import com.droidcon.android.MainActivity
 import com.droidcon.testing.R
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class ConferenceEndToEndTest {
 
-    @get:Rule
-    val rule: ActivityTestRule<MainActivity> =
-        ActivityTestRule(MainActivity::class.java, true, false)
-
     @Test
     fun givenRegisteredConferenceWhenApplicationStartsShowConferenceName() {
 
-        rule.launchActivity(Intent())
+        ActivityScenario.launch(MainActivity::class.java)
 
         Thread.sleep(2000)
 
--- conference-android/app/src/test/java/com/droidcon/conference/ConferenceComponentTest.kt
+++ conference-android/app/src/test/java/com/droidcon/conference/ConferenceComponentTest.kt
@@ -1,27 +1,24 @@
 package com.droidcon.conference
 
+import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.rule.ActivityTestRule
+import androidx.test.platform.app.InstrumentationRegistry
 import com.droidcon.Droidcon
-import com.droidcon.TestActivity
 import com.droidcon.android.AndroidApplication
+import com.droidcon.android.MainActivity
 import com.droidcon.testing.R
 import kotlinx.coroutines.Dispatchers
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class ConferenceComponentTest {
-
-    @get:Rule
-    val rule: ActivityTestRule<TestActivity> = ActivityTestRule(TestActivity::class.java)
 
     @Test
     fun `Given a registered conference When conference screen appears Then show conference name`() {
@@ -38,9 +35,9 @@ class ConferenceComponentTest {
 
         server.enqueue(MockResponse().setResponseCode(200).setBody(json))
 
-        val droidcon = Droidcon(Dispatchers.Unconfined, baseUrl)
-        (rule.activity.application as AndroidApplication).droidcon = droidcon
-        rule.activity.showFragment(ConferenceFragment())
+        (InstrumentationRegistry.getInstrumentation().targetContext as AndroidApplication)
+            .droidcon = Droidcon(Dispatchers.Unconfined, baseUrl)
+        ActivityScenario.launch(MainActivity::class.java)
 
         onView(withId(R.id.conferenceName)).check(matches(ViewMatchers.withText("Droidcon")))
 
